5a61b13013dc5782a49343ef0a548f19816a3cc0
Théo Weimann <theo.weimann.priv@gmail.com>
2022-02-06 17:58:54 -0500

    Refined the MathematicsTest method to be class dependent

--- src/test/java/MathematicsTest.java
+++ src/test/java/MathematicsTest.java
@@ -1,20 +1,12 @@
-import ca.uqam.tp3_mgl869c.*;
+import ca.uqam.tp3_mgl869c.Mathematics;
 import ca.uqam.tp3_mgl869c.polynomial.APolynomial;
 import ca.uqam.tp3_mgl869c.polynomial.Polynomial;
 import ca.uqam.tp3_mgl869c.polynomial.term.ExceptionCoefficientZero;
 import ca.uqam.tp3_mgl869c.polynomial.term.Term;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class MathematicsTest {
-    
-    Mathematics mathematics;
-    
-    @Before
-    public void initialize() {
-        mathematics = new Mathematics();
-    }
     
     @Test
     public void testSimplify() throws ExceptionCoefficientZero {
@@ -26,7 +18,7 @@ public class MathematicsTest {
         polynomial.addTerm(new Term(-20, 'y', 2));
         polynomial.addTerm(new Term(2, 'y', 10));
         polynomial.addTerm(new Term(9, 'y', 10));
-        mathematics.simplify(polynomial);
+        Mathematics.simplify(polynomial);
         String result   = polynomial.toString();
         String expected = "-2.5+2x+8x^2+-20y^2+11y^10";
         Assert.assertEquals(expected, result);
@@ -34,8 +26,8 @@ public class MathematicsTest {
     
     @Test
     public void testSumPolynomial() throws ExceptionCoefficientZero {
-        Polynomial p1= new APolynomial();
-        Polynomial p2= new APolynomial();
+        Polynomial p1 = new APolynomial();
+        Polynomial p2 = new APolynomial();
         p1.addTerm(new Term(1.5, ' ', 0));
         p1.addTerm(new Term(-4, ' ', 0));
         p1.addTerm(new Term(2, 'x', 1));
@@ -43,9 +35,9 @@ public class MathematicsTest {
         p2.addTerm(new Term(-20, 'y', 2));
         p2.addTerm(new Term(2, 'y', 10));
         p2.addTerm(new Term(9, 'y', 10));
-        Polynomial p3 = mathematics.sumPolynomials(p1,p2);
-        String result   = p3.toString();
-        String expected = "-2.5+2x+8x^2+-20y^2+11y^10";
+        Polynomial p3       = Mathematics.sumPolynomials(p1, p2);
+        String     result   = p3.toString();
+        String     expected = "-2.5+2x+8x^2+-20y^2+11y^10";
         Assert.assertEquals(expected, result);
     }
 }
